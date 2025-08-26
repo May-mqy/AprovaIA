81ebfab1539d4f3e0bdc680627b5129616a0a463
Gustavo Moura <gustavomouradejesus@gmail.com>
2025-08-26 00:31:58 -0300

    atualizações

--- assets/JS/patrocinadores.js
+++ assets/JS/patrocinadores.js
@@ -135,3 +135,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 // Fim seção doações 
 
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => {
+    const header = accordion.querySelector('.accordion-header');
+    
+    header.addEventListener('click', () => {
+        const body = accordion.querySelector('.accordion-body');
+        const icon = header.querySelector('.icon');
+
+        body.classList.toggle('active');
+
+        if (body.classList.contains('active')) {
+            icon.textContent = '–'; // Sinal de menos
+        } else {
+            icon.textContent = '+';
+        }
+    });
+});
